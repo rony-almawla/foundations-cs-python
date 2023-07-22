2033e28e5181baff2decc4ad7c517d408dee83b9
rony-almawla <ronyalmawla@gmail.com>
2023-07-22 09:22:15 +0300

    assignment_03_Ront_Al_Mawla
    assignment_03

---
+++ fsc course/assignment_03_Rony_Al_Mawla.py
@@ -0,0 +1,42 @@
+QUESTION 1:
+
+def sumTuples(tup1, tup2) :
+	tup1 = []
+	tup2 = []
+	tup1 = int(input("Enter any numbers you want"))
+	tup2 = int(input("Enter any numbers you want"))
+	tup3 = []
+	for i in range(length.tup1) and j in range(length.tup2) :
+		tup3 = [tup1(i) + tup2(j)]
+		i +=1
+		j+ =1
+	tup3 = tuple()
+return(tup3)
+
+
+
+def main():
+  num = int(input("Choose a number between 1 till 4"))
+  if num == 1:
+    sumTuples()
+  elif num ==2 : 
+    exportJson()
+  elif num ==3:
+    ImportJson()
+  elif num == 4:
+    exit()
+  else : 
+    print("please choose a number between 1 to 4")
+
+main()
+
+
+EXERCISE"
+a.O(1)
+b.O(N^2)
+c.O(N^2)
+d.o(1)
+e.O(N)
+f.O(N)
+g.O(1)
+h.O
